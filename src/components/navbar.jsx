03e813fe45ef5eb482bc59dbc0caf09344820d93
I Komang Adi Wirata <mangadiwirata578@gmail.com>
2025-08-03 15:00:50 +0800

    tema dark

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
 
     const isTransparent = location.pathname === '/' && !isScrolled;
     return (
-        <nav className={`fixed w-full z-30 top-0 start-0 transition-all duration-300 ${isTransparent ? 'py-2 bg-transparent' : 'py-2 bg-white  dark:border-gray-600'}`}>
+        <nav className={`fixed w-full z-30 top-0 start-0 transition-all duration-300 ${isTransparent ? 'py-2 bg-transparent' : 'py-2 bg-white dark:bg-gray-900  dark:border-gray-600'}`}>
             <div className="max-w-screen-xl flex flex-wrap md:flex-nowrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse md:order-1">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">World <span class="text-blue-700">Atlas</span></span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import HeroImage from '../assets/images/hero2.png';
 
 export const Home = () => {
   return (
-    <section className="homepage min-h-screen flex items-center pt-24 pb-12 lg:px-30">
+    <div className="homepage min-h-screen flex items-center pt-24 pb-12 lg:px-30">
       <div className="container mx-auto px-4">
         <div className="hero flex flex-col lg:flex-row items-center gap-12 lg:gap-16">
           <div className="text-container flex flex-col items-center lg:items-start text-center lg:text-left lg:w-1/2">
@@ -19,14 +19,48 @@ export const Home = () => {
             </div>
           </div>
           <div className="image-container order-2 lg:order-3 lg:w-1/2 flex justify-center mt-0 lg:mt-0">
-            <img 
-              src={HeroImage} 
-              alt="Hero Image" 
+            <img
+              src={HeroImage}
+              alt="Hero Image"
               className="w-full max-w-xs sm:max-w-sm lg:max-w-full rounded-lg" />
           </div>
         </div>
+
+
+
+
+        <div className="">
+          <div className="h-auto mt-6">
+            <h1 className="text-center text-2xl lg:text-3xl font-medium dark:text-white">Jelajahi <span className="text-blue-700">250+</span> Negara</h1>
+
+          
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto mt-12">
+              <div class="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition overflow-hidden group dark:bg-gray-800">
+                <div class="relative">
+                  <img src="https://plus.unsplash.com/premium_photo-1680346529160-549ad950bd1f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Product Image" class="w-full h-60 object-cover group-hover:scale-105 transition-transform" />
+                </div>
+                <div class="p-5">
+                  <h3 class="text-lg font-bold text-gray-900 mb-1 dark:text-white">Indonesia</h3>
+                  <p class="text-sm text-gray-500 mb-2 dark:text-gray-400">Memiliki 37 Provinsi</p>
+                  <div class="flex justify-between items-center mt-4">
+                    <span class="text-lg font-semibold text-blue-700">200jt Penduduk</span>
+                    <button class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 dark:bg-blue-700 dark:hover:bg-blue-500">Lihat</button>
+                  </div>
+                </div>
+              </div>
+
+             
+
+            </div>
+
+
+
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
+
+
 
 
   )
